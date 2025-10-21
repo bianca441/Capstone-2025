658b1d7826f661ee8d8e140bdfbf66f8608fbc7d
Javier Solar <114122506+xKakashixpy@users.noreply.github.com>
2025-10-20 21:39:02 -0300

    Actualizacion HOME

--- Codigo/mysite/mysite/settings.py
+++ Codigo/mysite/mysite/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',  
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -120,3 +120,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
